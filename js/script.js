e96e442728520b47c5bd8f34bd250e863b0ef9e3
brunaprogrammer <brunabiologarj@gmail.com>
2021-09-05 18:55:30 -0300

    agora vai

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var carrinho = document.querySelector(".carrinho");
 var menuOpen = document.querySelector(".menu-open");
-var fecharCarrinho = document.querySelector("#fechar-carrinho")
-
+var fecharCarrinho = document.querySelector("#fechar-carrinho");
+var fecharPedido = document.querySelector(".fechar-pedido");
 carrinho.addEventListener("click", function (){
     menuOpen.style.display = "block";
 })
@@ -9,3 +9,7 @@ carrinho.addEventListener("click", function (){
 fecharCarrinho.addEventListener("click", function(){
     menuOpen.style.display = "none";
 })
+
+fecharPedido.addEventListener("click", function(){
+    menuOpen.style.display = "none"
+} )
